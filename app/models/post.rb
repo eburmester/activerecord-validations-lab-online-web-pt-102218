c876d7e55f69b09fdaa0b037f6ea1ee3709b3ad5
Elyse Burmester <burmesterelyse@gmail.com>
2019-04-21 01:05:58 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ActiveRecord::Base
   validates :title, presense: true 
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
-  validates :category, inclusion: 
+  validates :category, inclusion: { in: %w(fiction non-fiction) }
+  validates :title, exclusion: { 
 end
