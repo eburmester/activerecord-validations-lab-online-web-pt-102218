9bafb5a1fbffb93f9bfd9ccf1af1615938f96d3d
Elyse Burmester <burmesterelyse@gmail.com>
2019-04-21 01:16:10 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,4 @@
 class Author < ActiveRecord::Base
-  validates :name, presense: true 
-  validates :name, uniqueness: :true 
+  validates :name, presense: true, uniqueness: :true
   validates :phone_number, length: { is: 10 }
 end
