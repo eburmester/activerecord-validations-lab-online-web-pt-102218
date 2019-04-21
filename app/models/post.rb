07b31448db582dc47c92b2cdfa43efd3c96c740e
Elyse Burmester <burmesterelyse@gmail.com>
2019-04-21 01:19:53 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(fiction non-fiction) }
-  validates :title, exclusion: { in: %w(wont\believe secret top[number] guess) }
+  validates :title, exclusion: { in: %w(/\b(Won't Believe|Secret|Top \d|Guess)\b/) }
 end
